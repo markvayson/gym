94dc842dfedcb2627acae9661af6a37338525ef0
markvayson <markvayson@gmail.com>
2023-08-16 02:37:47 +0400

    init

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -38,7 +38,11 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
 
   return (
     <nav ref={scope}>
-      <div className={` ${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${
+          !isTopOfPage && isAboveMediumScreens && "bg-white shadow-md"
+        } ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <motion.div
